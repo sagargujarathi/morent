0e7be5dab6f35913b1ec31f82a54d083caad211e
Sagar Gujarathi <110914955+sagargujarathi@users.noreply.github.com>
2024-04-24 01:49:29 -0700

    Update index.tsx

--- app/sections/nav-bar/index.tsx
+++ app/sections/nav-bar/index.tsx
@@ -9,6 +9,7 @@ import {
   AppBar,
   Avatar,
   Badge,
+  Container,
   IconButton,
   InputAdornment,
   Stack,
@@ -27,60 +28,64 @@ const Header = () => {
       }}
       position="fixed"
     >
-      <Stack
-        component="nav"
-        direction="row"
-        py={40 / 8}
-        px={60 / 8}
-        justifyContent="space-between"
-      >
-        <Stack direction="row" gap={8} alignItems="center">
-          <Typography
-            component="h1"
-            sx={{ fontSize: 32, fontWeight: 700 }}
-            color="primary"
-          >
-            MORENT
-          </Typography>
-          <TextField
-            sx={{ width: { lg: 500 } }}
-            placeholder="Search something here"
-            color="secondary"
-            size="small"
-            InputProps={{
-              sx: { borderRadius: 70 / 8, fontSize: 14 },
-              startAdornment: (
-                <InputAdornment position="start">
-                  <IconButton edge="start" size="small">
-                    <SearchIcon />
-                  </IconButton>
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton edge="end" size="small">
-                    <LinearFilter />
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
+      <Container maxWidth="xl"
+        sx={{
+          px: { lg: "64px !important" },
+        }}>
+        <Stack
+          component="nav"
+          direction="row"
+          py={40 / 8}
+          justifyContent="space-between"
+        >
+          <Stack direction="row" gap={8} alignItems="center">
+            <Typography
+              component="h1"
+              sx={{ fontSize: 32, fontWeight: 700 }}
+              color="primary"
+            >
+              MORENT
+            </Typography>
+            <TextField
+              sx={{ width: { lg: 500 } }}
+              placeholder="Search something here"
+              color="secondary"
+              size="small"
+              InputProps={{
+                sx: { borderRadius: 70 / 8, fontSize: 14 },
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton edge="start" size="small">
+                      <SearchIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton edge="end" size="small">
+                      <LinearFilter />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Stack>
+          <Stack direction="row" gap={20 / 8} alignItems="center">
+            <IconButton sx={{ border: `1px solid ${alpha("#C3D4E9", 0.4)}` }}>
+              <HeartIcon />
+            </IconButton>
+            <IconButton sx={{ border: `1px solid ${alpha("#C3D4E9", 0.4)}` }}>
+              <Badge color="error" badgeContent="" variant="dot">
+                <BellIcon />
+              </Badge>
+            </IconButton>
+            <IconButton sx={{ border: `1px solid ${alpha("#C3D4E9", 0.4)}` }}>
+              <SettingIcon />
+            </IconButton>
+            <Avatar />
+          </Stack>
         </Stack>
-        <Stack direction="row" gap={20 / 8} alignItems="center">
-          <IconButton sx={{ border: `1px solid ${alpha("#C3D4E9", 0.4)}` }}>
-            <HeartIcon />
-          </IconButton>
-          <IconButton sx={{ border: `1px solid ${alpha("#C3D4E9", 0.4)}` }}>
-            <Badge color="error" badgeContent="" variant="dot">
-              <BellIcon />
-            </Badge>
-          </IconButton>
-          <IconButton sx={{ border: `1px solid ${alpha("#C3D4E9", 0.4)}` }}>
-            <SettingIcon />
-          </IconButton>
-          <Avatar />
-        </Stack>
-      </Stack>
+      </Container>
     </AppBar>
   );
 };
